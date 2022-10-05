47b767384affd42aaeb0a902841fd668a316e618
that guy <trey.hitchcock@gmail.com>
2022-10-05 01:15:55 -0700

    Created Editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import * as esbuild from 'esbuild-wasm';
 import { createRoot } from 'react-dom/client';
 import { useEffect, useRef, useState } from "react";
-import React from "react";
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
+import MonacoEditor from './components/code-editor';
 
 const App = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
 
@@ -24,23 +26,54 @@ const App = () => {
             return;
         }
 
+        iframe.current.srcdoc = html;
+
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin()],
+            plugins: [
+                unpkgPathPlugin(),
+                fetchPlugin(input),
+            ],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window',
+            },
         });
     
-        setCode(result.outputFiles[0].text);
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     };
+
+    const html = `
+    <html>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    try {
+                        eval(event.data);
+                    } catch (err) {
+                        const root = document.querySelector('#root');
+                        root.innerHTML = '<div style="color:red;"><h4>Runtime Error</h4>' + err + '</div>';
+                        throw err;
+                    }
+                }, false);
+            </script>
+        </body>
+    </html>
+    `;
 
     return (
         <div>
+            <MonacoEditor initialValue='Enter in some Code!'/>
             <textarea value={input} onChange={e=> setInput(e.target.value)}></textarea>
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
             <pre>{code}</pre>
+            <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
         </div>
     );
 };
